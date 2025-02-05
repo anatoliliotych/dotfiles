1f25501a9a36ad67bb79771ec7f22d955a87f92b
Anatoli Liotych <anatoli.liotych@gmail.com>
2025-02-05 10:18:41 +0100

    Change font_size for WezTerm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,23 +1,17 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 local act = wezterm.action
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = 'OneHalfDark'
 
--- How many lines of scrollback you want to retain per tab
 config.scrollback_lines = 50000
+
+config.font_size = 22
 
 config.keys = {
 { key = 'k', mods = 'CMD', action = act.ClearScrollback 'ScrollbackAndViewport', },
 { key = '/', mods = 'CTRL', action=wezterm.action.SendString("\x1f") },
 }
 
-
--- and finally, return the configuration to wezterm
 return config
