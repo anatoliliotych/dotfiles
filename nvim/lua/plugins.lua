8916a4be6df79861816381724937969fa16efaab
Anatoli Liotych <anatoli.liotych@gmail.com>
2025-07-10 09:27:42 +0100

    Remove unneeded plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,10 +2,10 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', vim.fn.expand('~/.config/nvim/plugged'))
 
 Plug 'bling/vim-airline'
-Plug 'tpope/vim-fugitive'
 Plug 'airblade/vim-gitgutter'
 Plug 'ggml-org/llama.vim'
 Plug 'ibhagwan/fzf-lua'
+
 Plug('sonph/onehalf', { rtp = 'vim' })
 
 vim.call('plug#end')
